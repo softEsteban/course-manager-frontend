d98bab96d62e6c88608652f8379a64e0cad3bd2e
Esteban Toro <estebantoro.greenman@gmail.com>
2022-01-31 06:04:52 +0100

    angular project basic structure

---
+++ src/app/modules/school-info/school-info.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SchoolInfoRoutingModule } from './school-info-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SchoolInfoRoutingModule
+  ]
+})
+export class SchoolInfoModule { }
